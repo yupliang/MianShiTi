6a9c1a8555fa9c6599a9072d9f919be8b7268799
yupeiliang <yupeiliang@fotoable.com>
2019-06-14 15:17:46 +0800

    循环队列初探，通过head 和tail两个下标的移动使得 头删尾插得队列避免了假溢出的问题；实质是在tail下标达到最大时，将要移动的下个坐标调回到0，再依次1,2,3…

---
+++ Questions/MyCircleQueue.swift
@@ -0,0 +1,92 @@
+//
+//  MyCircleQueue.swift
+//  MianshiTi
+//
+//  Created by yupeiliang on 2019/6/14.
+//  Copyright © 2019 QiQiuZhe. All rights reserved.
+//
+
+import Foundation
+
+
+class MyCircularQueue {
+    
+    var data:[Int]!
+    var size:Int!
+    var empty:Bool!
+    var head:Int!
+    var tail:Int!
+    /** Initialize your data structure here. Set the size of the queue to be k. */
+    init(_ k: Int) {
+        empty = true
+        size = k
+        data = Array<Int>()
+        for _ in 0...k {
+            data.append(-1)
+        }
+        head = 0
+        tail = 0
+    }
+    
+    /** Insert an element into the circular queue. Return true if the operation is successful. */
+    func enQueue(_ value: Int) -> Bool {
+        if !isFull() {
+            data[tail] = value
+            tail = (tail+1)%size
+            empty = false
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    /** Delete an element from the circular queue. Return true if the operation is successful. */
+    func deQueue() -> Bool {
+        if !isEmpty() {
+            head = head+1
+            data[head] = -1
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    /** Get the front item from the queue. */
+    func Front() -> Int {
+        if !isEmpty() {
+            return data[head]
+        } else {
+            return -1
+        }
+    }
+    
+    /** Get the last item from the queue. */
+    func Rear() -> Int {
+        if !isEmpty() {
+            return data[(tail+size-1)%size]
+        } else {
+            return -1
+        }
+    }
+    
+    /** Checks whether the circular queue is empty or not. */
+    func isEmpty() -> Bool {
+        return empty
+    }
+    
+    /** Checks whether the circular queue is full or not. */
+    func isFull() -> Bool {
+        return head == tail && isEmpty() == false
+    }
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * let obj = MyCircularQueue(k)
+ * let ret_1: Bool = obj.enQueue(value)
+ * let ret_2: Bool = obj.deQueue()
+ * let ret_3: Int = obj.Front()
+ * let ret_4: Int = obj.Rear()
+ * let ret_5: Bool = obj.isEmpty()
+ * let ret_6: Bool = obj.isFull()
+ */
