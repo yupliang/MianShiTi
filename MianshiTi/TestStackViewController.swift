3e5aea8af1982e29111cc159db0a551e9ad0b6e5
yupeiliang <yupeiliang@fotoable.com>
2019-07-12 20:47:24 +0800

    测试uistackview

---
+++ MianshiTi/TestStackViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TestStackViewController.swift
+//  MianshiTi
+//
+//  Created by yupeiliang on 2019/7/12.
+//  Copyright © 2019 QiQiuZhe. All rights reserved.
+//
+
+import UIKit
+
+class TestStackViewController: UIViewController {
+
+    @IBOutlet var weatherInfo: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func act(_ sender: Any) {
+        weatherInfo.isHidden = !weatherInfo.isHidden;
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
