0ac2b46f3f4d56cfda44fe72d23a5593c362eb25
yupliang <>
2019-08-28 22:04:52 +0800

    将oc文件转成c++\指定平台、指定架构

---
+++ MJ/mjmain.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CTool
+//
+//  Created by Qiqiuzhe on 2019/8/28.
+//  Copyright © 2019 ToolMaker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
